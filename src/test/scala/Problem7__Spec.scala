b567baa8bd48f82c24c08248433eb7fb0c5a2231
Kenny Vo <kennyvo@Kennys-MacBook-Pro.local>
2019-10-29 14:25:05 +0000

    Done P7 with unit tests

---
+++ src/test/scala/Problem7__Spec.scala
@@ -0,0 +1,10 @@
+import org.scalatest.{FlatSpec, Matchers}
+
+class Problem7__Spec extends FlatSpec with Matchers {
+
+  "countDecodings" should "return positive count of decodings" in {
+    Problem7.countDecodings("111") should be (3)
+    Problem7.countDecodings("1815") should be (4)
+    Problem7.countDecodings("207693") should be (5)
+  }
+}
