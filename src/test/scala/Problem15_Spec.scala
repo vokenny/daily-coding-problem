acef1a6dd2f61cba68c5b2bb1d8cf59a25364358
Kenny Vo <kennyvo@Kennys-MacBook-Pro.local>
2019-11-15 15:48:35 +0000

    Done Problem 15 with unit tests

---
+++ src/test/scala/Problem15_Spec.scala
@@ -0,0 +1,15 @@
+import org.scalatest.{FlatSpec, Matchers}
+
+class Problem15_Spec extends FlatSpec with Matchers {
+
+  val streamStr: LazyList[Int] = LazyList.tabulate(20)(e => e + 1)
+  val streamStrEmpty: LazyList[Int] = LazyList.tabulate(0)(e => e + 1)
+
+  "randomFromStream" should "return uniformly distributed random integer" in {
+    Problem15.randomFromStream(streamStr).isInstanceOf[Int] should be (true)
+  }
+
+  it should "throw IllegalArgumentException for empty stream" in {
+    an [IllegalArgumentException] should be thrownBy Problem15.randomFromStream(streamStrEmpty)
+  }
+}
