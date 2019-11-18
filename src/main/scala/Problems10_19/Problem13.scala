cfcd8ac02db634f0721f0840db0a1d60c5316059
Kenny Vo <kennyvo@Kennys-MacBook-Pro.local>
2019-11-18 13:33:07 +0000

    Done P13 with unit tests

--- src/main/scala/Problems10_19/Problem13.scala
+++ src/main/scala/Problems10_19/Problem13.scala
@@ -1,5 +1,7 @@
 package Problems10_19
 
+import scala.annotation.tailrec
+
 object Problem13 extends App {
 
   // HARD
@@ -9,4 +11,41 @@ object Problem13 extends App {
   //
   // For example, given s = "abcba" and k = 2, the longest substring with k distinct characters is "bcb".
 
+  // counter following original query
+  // param for current chars
+  // param for remaining chars
+  // param for tracking longest substring overall
+
+  val example = "abcba"
+
+  def lessOrEqualToUniqueChars(query: List[Char], n: Int): Boolean = {
+    @tailrec
+    def checkUniqueChars(count: Int, remain: List[Char]): Boolean = {
+      (count, remain) match {
+        case (_, Nil) => true
+        case (0, r) => r.isEmpty
+        case (c, r) => checkUniqueChars(c - 1, r.filter(c => c != r.head))
+      }
+    }
+
+    if (n > 0) checkUniqueChars(n, query) else throw new IllegalArgumentException
+  }
+
+  def takeLarger(a: List[Char], b: List[Char]): List[Char] = if (a.size > b.size) a else b
+
+  def longestSubStrUniqueChars(query: String, unique: Int): String = {
+    @tailrec
+    def longestSubStr(count: Int, current: List[Char], remain: List[Char], longest: List[Char]): String = {
+      if (remain.isEmpty) longest.reverse.mkString
+      else {
+        if (lessOrEqualToUniqueChars(remain.head :: current, unique)) {
+          longestSubStr(count, remain.head :: current, remain.tail, takeLarger(remain.head :: current, longest))
+        } else longestSubStr(count + 1, Nil, query.drop(count + 1).toList, longest)
+      }
+    }
+
+    longestSubStr(0, Nil, query.toList, Nil)
+  }
+
+  println(longestSubStrUniqueChars(example, 2))
 }
---
+++ src/test/scala/Problem13_Spec.scala
@@ -0,0 +1,53 @@
+import Problems10_19.Problem13
+import org.scalatest.{FlatSpec, Matchers}
+
+class Problem13_Spec extends FlatSpec with Matchers {
+
+  "lessOrEqualToUniqueChars" should "return true for empty an list" in {
+    Problem13.lessOrEqualToUniqueChars(Nil, 2) should be (true)
+  }
+
+  it should "return true for a list of chars with fewer unique characters than n" in {
+    Problem13.lessOrEqualToUniqueChars(List('x', 'y', 'x', 'x', 'y'), 3) should be (true)
+    Problem13.lessOrEqualToUniqueChars(List('x', 'y', 'x', 'x', 'y'), 10) should be (true)
+  }
+
+  it should "return true for a list of chars with equal number of unique characters to n" in {
+    Problem13.lessOrEqualToUniqueChars(List('x', 'y', 'x', 'x', 'y'), 2) should be (true)
+  }
+
+  it should "return false for a list of chars with more unique characters than n" in {
+    Problem13.lessOrEqualToUniqueChars(List('x', 'y', 'z'), 2) should be (false)
+  }
+
+  it should "throw IllegalArgumentException for n < 1" in {
+    an [IllegalArgumentException] should be thrownBy Problem13.lessOrEqualToUniqueChars(List('x', 'y', 'z'), 0)
+  }
+
+  "takeLarger" should "return larger list" in {
+    Problem13.takeLarger(List('a', 'b', 'c'), List('a', 'b')) should be (List('a', 'b', 'c'))
+    Problem13.takeLarger(List('a', 'b'), List('a', 'b', 'c')) should be (List('a', 'b', 'c'))
+  }
+
+  it should "return the latter option if they are equal in size" in {
+    Problem13.takeLarger(List('a', 'b', 'c'), List('x', 'y', 'z')) should be (List('x', 'y', 'z'))
+  }
+
+  it should "return Nil for comparing empty lists" in {
+    Problem13.takeLarger(List(), Nil) should be (Nil)
+    Problem13.takeLarger(Nil, List()) should be (Nil)
+  }
+
+  "longestSubStrUniqueChars" should "return the longest substring with the specific unique number of characters" in {
+    Problem13.longestSubStrUniqueChars("", 2) should be ("")
+    Problem13.longestSubStrUniqueChars("aaaxyyz", 2) should be ("aaax")
+    Problem13.longestSubStrUniqueChars("aaaxyxyyz", 2) should be ("xyxyy")
+    Problem13.longestSubStrUniqueChars("aaaxyxyyzzyz", 2) should be ("yyzzyz")
+    Problem13.longestSubStrUniqueChars("aaaxyxyyzzyz", 4) should be ("aaaxyxyyzzyz")
+    Problem13.longestSubStrUniqueChars("aaaxyxyyzzyz", 5) should be ("aaaxyxyyzzyz")
+  }
+
+  it should "throw IllegalArgumentException for zero unique num of characters" in {
+    an [IllegalArgumentException] should be thrownBy Problem13.longestSubStrUniqueChars("aaaxyyz", 0)
+  }
+}
