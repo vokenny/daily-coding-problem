c2506b1a2e304a560f44da1ad65c4c1ad372ef04
Kenny Vo <kennyvo@Kennys-MacBook-Pro.local>
2020-02-04 16:08:03 +0000

    Working on P33

--- src/main/scala/Problems30_39/Problem33.scala
+++ src/main/scala/Problems30_39/Problem33.scala
@@ -1,5 +1,7 @@
 package Problems30_39
 
+import scala.annotation.tailrec
+
 object Problem33 extends App {
 
   // EASY
@@ -20,4 +22,34 @@ object Problem33 extends App {
   // 2
   // 2
 
+  def printRunningMedian(sequence: List[Int]): Unit = {
+    @tailrec
+    def printMedian(runSeq: List[Int], remSeq: List[Int]): Unit = {
+      remSeq match {
+        case Nil => println(runSeq.sorted)
+        case h :: Nil =>
+          println(runSeq.sorted)
+          printMedian(h :: runSeq, Nil)
+        case h :: tail =>
+          println(runSeq.sorted)
+          printMedian(h :: runSeq, tail)
+      }
+    }
+
+    printMedian(Nil, sequence)
+  }
+
+  def printRunningMean(sequence: List[Int]): Unit = {
+    sequence.foldLeft((0.0, 0.0, 0.0)) { (r, s) => {
+      val sum: Double = r._1 + s
+      val count: Double = r._3 + 1
+      val med: Double = sum/count
+      println(med)
+      (sum, med, count)
+    }}
+  }
+
+  val seq = List(2, 1, 5, 7, 2, 0, 5)
+
+  printRunningMedian(seq)
 }
