794211edc340fceb3629ed8f15785af6cbeef948
Kenny Vo <28298773+vokenny@users.noreply.github.com>
2020-11-01 22:12:23 +0000

    Done P12

--- src/main/scala/hard/Problem12.scala
+++ src/main/scala/hard/Problem12.scala
@@ -2,6 +2,10 @@ package hard
 
 object Problem12 extends App {
 
+  // 2020-11-01
+  // N.B. Watched Dynamic programming solution to understand the problem & solution
+  // https://www.youtube.com/watch?v=5o-kdjv7FD0
+
   // HARD
   // This problem was asked by Amazon.
   //
@@ -19,5 +23,19 @@ object Problem12 extends App {
   //
   // What if, instead of being able to climb 1 or 2 steps at a time, you could climb any number
   // from a set of positive integers X? For example, if X = {1, 3, 5}, you could climb 1, 3, or 5 steps at a time.
+
+  def stepCountVariations(steps: Int, stepSizes: List[Int]): Int = {
+    if (steps == 0) 1
+    else {
+      for (step <- stepSizes) yield {
+        val remainingSteps = steps - step
+        if (remainingSteps >= 0) stepCountVariations(remainingSteps, stepSizes) else 0
+      }
+    }.sum
+  }
+
+  println(stepCountVariations(4, List(1, 2)))
+  println(stepCountVariations(4, List(1, 2, 3)))
+  println(stepCountVariations(5, List(1, 3, 5)))
 
 }
