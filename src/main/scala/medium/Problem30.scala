77934f4bc44cfab3deb0163c4ebc80e2287f95ca
Kenny Vo <28298773+vokenny@users.noreply.github.com>
2020-11-01 20:22:20 +0000

    Edit comment on P30

--- src/main/scala/medium/Problem30.scala
+++ src/main/scala/medium/Problem30.scala
@@ -15,10 +15,10 @@ object Problem30 extends App {
   // Given the input [3, 0, 1, 3, 0, 5], we can hold 3 units in the first index, 2 in the second,
   // and 3 in the fourth index (we cannot hold 5 since it would run off to the left), so we can trap 8 units of water.
   //
-  //          |
-  //          |
-  // |     |  |
-  // |     |  |
-  // |  |  |  |
+  //      |
+  //      |
+  // |  | |
+  // |  | |
+  // |_||_|
 
 }
